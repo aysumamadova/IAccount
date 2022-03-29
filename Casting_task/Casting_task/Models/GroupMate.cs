65e04c29293a46a64c09cab5c0d80852219d20b8
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-29 19:13:56 +0400

    add

---
+++ Casting_task/Casting_task/Models/GroupMate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casting_task.Models
+{
+    class GroupMate
+    {
+        Student[] students = { };
+    }
+}
---
+++ Casting_task/Casting_task/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Casting_task.Models;
+
+namespace Casting_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(23);
+            Student s2 = new Student(25);
+            Teacher t1 = new Teacher(230);
+            Teacher t2 = new Teacher(250);
+        }
+    }
+}
