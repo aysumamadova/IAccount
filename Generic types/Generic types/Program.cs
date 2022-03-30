ceba4f0a7999d1cf0b9d2967d01529bc3c9f09f0
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-30 22:12:40 +0400

    add

---
+++ Generic types/Generic types/Program.cs
@@ -0,0 +1,35 @@
+﻿using Generic_types.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Generic_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> mylist = new MyList<string>();
+            mylist.Add("Aysu");
+            mylist.Add("ghjjj"); 
+            mylist.Add("task"); 
+            mylist.Add("pdf"); 
+            mylist.Insert(9,"747");
+            //list.Clear();
+            //- Exist()
+            //- Remove()
+            //- Reverse()
+            //- IndexOf()
+            
+            foreach (var item in mylist) 
+            {
+
+                Console.WriteLine(item);
+
+
+            }
+
+            Console.WriteLine(mylist.Count);
+
+        }
+    }
+}
